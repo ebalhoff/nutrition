14962e9699463905679d008e69499a8bcce5287c
pdjnwydpj7qz <elisebalhoff@gmail.com>
2025-06-05 00:11:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from sqlalchemy import create_engine, text
 from os import getenv
 
 app = Flask(__name__)
-engine = create_engine(getenv('POSTGRES_URL'), echo=False)
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
 
 @app.route('/usda')
 def index():
